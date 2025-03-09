b85f1396735747a42aded533057084738ccee3a6
anarhisssttt <grebeneva.y.i.23@gmail.com>
2025-03-09 12:50:16 +0300

    yasya_2_6

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>
+    <v-row justify="center">
+      <h1>Home</h1>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  data () {
+    return {
+    }
   }
 }
 </script>
