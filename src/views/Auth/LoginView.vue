a51b033d2bab6c1befdb99585a7cadb437ef4952
anarhisssttt <grebeneva.y.i.23@gmail.com>
2025-03-09 13:08:46 +0300

    Добавление_форм_логина_3_3

--- src/views/Auth/LoginView.vue
+++ src/views/Auth/LoginView.vue
@@ -2,19 +2,23 @@
     <v-container>
       <v-row justify="center">
         <v-col cols="12" sm="8" lg="6">
-            <v-card class="elevetion-12" >
-                <v-toolbar dark color="primary" class="pl-4" >
-                    Login
+            <v-card class="elevetion-12">
+                <v-toolbar dark color="primary">
+                    <v-toolbar-title>
+                        Login
+                    </v-toolbar-title>
                 </v-toolbar>
                 <v-card-text>
-                <v-form>
-                <v-text-field prepend-icon="mdi-account" name="email" label="Email" type="email" v-model="email"></v-text-field>
-                <v-text-field prepend-icon="mdi-lock" name="password" label="Password" type="password" v-model="password"></v-text-field>
+                <v-form v-model="valid" ref="form" validation>
+                <v-text-field prepend-icon="mdi-account" name="email" label="Email" type="email" v-model="email" :rules="emailRules"></v-text-field>
+                <v-text-field prepend-icon="mdi-lock" name="password" label="Password" type="password" v-model="password" :rules="passwordRules"></v-text-field>
                 </v-form>
                 </v-card-text>
                 <v-card-actions>
                     <v-spacer></v-spacer>
-                    <v-btn color="primary">Login</v-btn>
+                    <v-btn color="primary">
+                        Login
+                    </v-btn>
                 </v-card-actions>
             </v-card>
         </v-col>
@@ -27,7 +31,17 @@
     data () {
       return {
         email:"",
-        password:""
+        password:"",
+        valid: false,
+        emailRules: [
+            v => !!v || 'E-mail is reqired',
+            v => /.+@.+\..+/.test(v) || 'E-mail must be valid'    
+        ],
+
+        passwordRules: [
+            v => !!v || 'Name is required',
+            v => (v && v.length >= 6) || 'Password must be more or equel than 6 characters',
+        ]
       }
     }
   }
