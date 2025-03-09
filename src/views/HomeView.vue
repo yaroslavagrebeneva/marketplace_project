1da2d9c0b6428329533f9551d9ce98fc72225771
anarhisssttt <grebeneva.y.i.23@gmail.com>
2025-03-09 13:19:53 +0300

    Подготовка данных для главного экрана

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -39,6 +39,36 @@
 export default {
   data () {
     return {
+      abs: [
+           {
+            title:"First",
+            desc:"First Desc",
+            promo: true,
+            src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+            id:"1"
+           },
+           {
+            title:"Second",
+            desc:"Second Desc",
+            promo: true,
+            src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
+            id:"2"
+           },
+           {
+            title:"Third",
+            desc:"Thitd Desc",
+            promo: true,
+            src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
+            id:"3"
+           },
+           {
+            title:"Fouth",
+            desc:"Fouth Desc",
+            promo: true,
+            src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
+            id:"4"
+           }
+      ]
     }
   }
 }
