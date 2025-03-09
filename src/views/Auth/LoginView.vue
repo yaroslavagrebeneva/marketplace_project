dc482dbe40a6131b1e71472dc46ede81324f7930
anarhisssttt <grebeneva.y.i.23@gmail.com>
2025-03-09 13:03:56 +0300

    Добавление_форм_логина_3_1

--- src/views/Auth/LoginView.vue
+++ src/views/Auth/LoginView.vue
@@ -1,7 +1,23 @@
 <template>
     <v-container>
       <v-row justify="center">
-        <h1>Login</h1>
+        <v-col cols="12" sm="8" lg="6">
+             <v-card class="elevetion-12" >
+                 <v-toolbar dark color="primary" class="pl-4" >
+                     Login
+                 </v-toolbar>
+                 <v-card-text>
+                 <v-form>
+                 <v-text-field prepend-icon="mdi-account" name="email" label="Email" type="email"></v-text-field>
+                 <v-text-field prepend-icon="mdi-lock" name="password" label="Password" type="password"></v-text-field>
+                 </v-form>
+                 </v-card-text>
+                 <v-card-actions>
+                     <v-spacer></v-spacer>
+                     <v-btn color="primary">Login</v-btn>
+                 </v-card-actions>
+             </v-card>
+         </v-col>
       </v-row>
     </v-container>
   </template>
