fa7e0fff775b42a1540e6080261f6be7f8b0b1d9
anarhisssttt <grebeneva.y.i.23@gmail.com>
2025-03-09 12:54:47 +0300

    yasya_2_7

---
+++ src/views/Ads/AdView.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-container>
+      <v-row justify="center">
+        <h1>Ad</h1>
+      </v-row>
+    </v-container>
+  </template>
+  
+  <script>
+  export default {
+    data () {
+      return {
+      }
+    }
+  }
+  </script>
---
+++ src/views/Auth/LoginView.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-container>
+      <v-row justify="center">
+        <h1>Login</h1>
+      </v-row>
+    </v-container>
+  </template>
+  
+  <script>
+  export default {
+    data () {
+      return {
+      }
+    }
+  }
+  </script>
