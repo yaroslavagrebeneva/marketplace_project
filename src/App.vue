451824e20e935b52bfb76801bf6471044519981d
anarhisssttt <grebeneva.y.i.23@gmail.com>
2025-03-09 12:45:25 +0300

    yasya_2_4

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,29 @@
 
 <template>
-  <v-app>
-  <v-navigation-drawer app v-model="drawer"></v-navigation-drawer> 
+  <v-app><v-navigation-drawer app v-model="drawer">
+ 
+       <v-list-item>
+       <v-list-item-content>
+             <v-list-item-title class="title">
+               КИПУ
+             </v-list-item-title>
+                 <v-list-item-subtitle>
+               Учебный проект
+             </v-list-item-subtitle>
+       </v-list-item-content>
+   </v-list-item>
+ 
+   <v-divider></v-divider>
+ 
+   <v-list dense>
+   <v-list-item>
+   <template v-slot:prepend>
+   <v-icon icon="mdi-cake-variant"></v-icon>
+   </template>
+   <v-list-item-title>Link One</v-list-item-title>
+   </v-list-item>
+   </v-list>
+ </v-navigation-drawer> 
   <v-app-bar app dark color="primary">
     <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
     <v-spacer></v-spacer>
