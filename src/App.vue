44ac6ec2ff0fedb839d5442b4ee21e11e509b578
anarhisssttt <grebeneva.y.i.23@gmail.com>
2025-03-09 12:49:02 +0300

    yasya_2_5

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,55 @@
-
 <template>
-  <v-app><v-navigation-drawer app v-model="drawer">
- 
-       <v-list-item>
-       <v-list-item-content>
-             <v-list-item-title class="title">
-               КИПУ
-             </v-list-item-title>
-                 <v-list-item-subtitle>
-               Учебный проект
-             </v-list-item-subtitle>
-       </v-list-item-content>
-   </v-list-item>
- 
-   <v-divider></v-divider>
- 
-   <v-list dense>
-   <v-list-item>
-   <template v-slot:prepend>
-   <v-icon icon="mdi-cake-variant"></v-icon>
-   </template>
-   <v-list-item-title>Link One</v-list-item-title>
-   </v-list-item>
-   </v-list>
- </v-navigation-drawer> 
-  <v-app-bar app dark color="primary">
-    <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+  <v-app>
+  <v-navigation-drawer app v-model="drawer"><v-list-item>
+    <v-list-item-content>
+          <v-list-item-title class="title">
+            КИПУ
+          </v-list-item-title><v-list-item-subtitle>
+            Учебный проект
+          </v-list-item-subtitle>
+    </v-list-item-content>
+</v-list-item>
+
+<v-divider></v-divider>
+
+<v-list dense>
+    <v-list-item v-for="link in links":key="link.title">
+    <template v-slot:prepend>
+    <v-icon icon="mdi-cake-variant"></v-icon>
+    </template>
+    <v-list-item-title>{{ link.title }}</v-list-item-title>
+    </v-list-item>
+</v-list>
+</v-navigation-drawer> 
+<v-app-bar app dark color="primary">
+<v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
     <v-spacer></v-spacer>
     <v-toolbar-items class="hidden-sm-and-down">
-       <v-btn>
-<v-icon
-start
-icon="mdi-cake-variant"
-></v-icon>
-Link One
-</v-btn>
-       <v-btn>Link Two</v-btn> 
-       <v-btn>Link Three</v-btn>
-    </v-toolbar-items>
-  </v-app-bar>
-  <v-content>
-    <router-view></router-view> 
-  </v-content>
-  </v-app>
+      <v-btn text v-for="link in links":key="link.title">
+<v-icon start:icon="link.icon"></v-icon>
+      {{ link.title }}
+    </v-btn>
+</v-toolbar-items>
+</v-app-bar>
+<v-main>
+</v-main>
+</v-app>
 </template>
+
 
 <script>
 export default {
   data() {
     return {
-      drawer: false
-    }
+      drawer:false,
+      links:[
+      {title:"Login", icon:"mdi-lock", url:"/login"},
+      {title:"Registration",icon:"mdi-face",url:"/registration"},
+      {title:"Orders",icon:"mdi-bookmark-multiple-outline", url:"/orders"},
+      {title:"New ad", icon:"mdi-note-plus-outline", url:"/new"},
+      {title:"My ads", icon:"mdi-view-list-outline", url:"/list"}
+      ]
+      }
+      }
   }
-}
 </script>
