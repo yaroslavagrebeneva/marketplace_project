cb443564b221979c6287c7f9435363c4b845b47b
anarhisssttt <grebeneva.y.i.23@gmail.com>
2025-03-09 12:41:40 +0300

    yasya_2_1

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,21 @@
-
 <template>
-  <h1>Очистили стартовый проект</h1>
-  </template>
-  
+  <v-app>
+  <v-navigation-drawer app></v-navigation-drawer> 
+  <v-app-bar app dark color="primary">
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+       <v-btn  text>Link One</v-btn> 
+       <v-btn  text>Link Two</v-btn> 
+       <v-btn  text>Link Three</v-btn>
+    </v-toolbar-items>
+  </v-app-bar>
+  <v-main>
+    
+  </v-main>
+  </v-app>
+</template>
+
   <script>
   export default {
   }
