6c1f49d25dac047c46e30db8ea860bcccacaa24f
anarhisssttt <grebeneva.y.i.23@gmail.com>
2025-03-09 12:43:03 +0300

    yasya_2_3

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,34 @@
+
 <template>
   <v-app>
-  <v-navigation-drawer app></v-navigation-drawer>
+  <v-navigation-drawer app v-model="drawer"></v-navigation-drawer> 
   <v-app-bar app dark color="primary">
-  <v-app-bar-nav-icon></v-app-bar-nav-icon>
-  <v-spacer></v-spacer>
-  <v-toolbar-items class="hidden-sm-and-down">
-  <v-btn>
-  <v-icon
-  start
-  icon="mdi-cake-variant"
-  ></v-icon>
-  Link One
-  </v-btn>
-  <v-btn>Link Two</v-btn>
-  <v-btn>Link Three</v-btn>
-  </v-toolbar-items>
+    <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+       <v-btn>
+<v-icon
+start
+icon="mdi-cake-variant"
+></v-icon>
+Link One
+</v-btn>
+       <v-btn>Link Two</v-btn> 
+       <v-btn>Link Three</v-btn>
+    </v-toolbar-items>
   </v-app-bar>
-  <v-main>
-  </v-main>
+  <v-content>
+    <router-view></router-view> 
+  </v-content>
   </v-app>
-  </template>
-  
+</template>
 
-  <script>
-  export default {
+<script>
+export default {
+  data() {
+    return {
+      drawer: false
+    }
   }
-  </script>
-  
+}
+</script>
