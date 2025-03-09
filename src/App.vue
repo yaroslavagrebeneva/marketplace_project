c2784bbca1b5385d5c89761899d506bd065e0e5e
anarhisssttt <grebeneva.y.i.23@gmail.com>
2025-03-09 12:42:27 +0300

    yasya_2_2

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,26 @@
 <template>
   <v-app>
-  <v-navigation-drawer app></v-navigation-drawer> 
+  <v-navigation-drawer app></v-navigation-drawer>
   <v-app-bar app dark color="primary">
-    <v-app-bar-nav-icon></v-app-bar-nav-icon>
-    <v-spacer></v-spacer>
-    <v-toolbar-items class="hidden-sm-and-down">
-       <v-btn  text>Link One</v-btn> 
-       <v-btn  text>Link Two</v-btn> 
-       <v-btn  text>Link Three</v-btn>
-    </v-toolbar-items>
+  <v-app-bar-nav-icon></v-app-bar-nav-icon>
+  <v-spacer></v-spacer>
+  <v-toolbar-items class="hidden-sm-and-down">
+  <v-btn>
+  <v-icon
+  start
+  icon="mdi-cake-variant"
+  ></v-icon>
+  Link One
+  </v-btn>
+  <v-btn>Link Two</v-btn>
+  <v-btn>Link Three</v-btn>
+  </v-toolbar-items>
   </v-app-bar>
   <v-main>
-    
   </v-main>
   </v-app>
-</template>
+  </template>
+  
 
   <script>
   export default {
