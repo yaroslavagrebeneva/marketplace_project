56a8070a1e7a5b0800324a1cfca24910604ee369
anarhisssttt <grebeneva.y.i.23@gmail.com>
2025-04-10 18:37:25 +0300

    Обработка клика на checkbox

--- src/views/User/OrdersView.vue
+++ src/views/User/OrdersView.vue
@@ -1,16 +1,54 @@
 <template>
-    <v-container>
+  <v-container>
       <v-row justify="center">
-        <h1>Orders</h1>
+          <v-col cols="12" sm="8" lg="6">
+              <h1 class="text--secondary mb-3 mt-3">Orders</h1>
+              <v-list subheader two-line flat>
+                  <v-list-item v-for="order in orders" :key="order.id">
+                      <template v-slot:prepend>
+                          <v-list-item-action>
+                              <v-checkbox :input-value="order.done" color="primary"
+                                  @click="markDone(order)"></v-checkbox>
+                          </v-list-item-action>
+                      </template>
+                      <v-list-item-title>{{ order.name }}</v-list-item-title>
+                      <v-list-item-subtitle>{{ order.phone }}</v-list-item-subtitle>
+
+                      <template v-slot:append>
+                          <v-list-item-action>
+                              <v-btn class="primary" :to="'/ad/' + order.adId">Open</v-btn>
+                          </v-list-item-action>
+                      </template>
+                  </v-list-item>
+              </v-list>
+
+
+          </v-col>
       </v-row>
-    </v-container>
-  </template>
-  
-  <script>
-  export default {
-    data () {
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
       return {
+          orders: [
+              {
+                  id: "123",
+                  name: "Kate",
+                  phone: "+7(978)774-57-96",
+                  adId: "1",
+                  done: true
+              }
+          ]
       }
-    }
-  }
-  </script>
+  },
+  methods: {
+      markDone(order) {
+          order.done = !order.done;
+          console.log(order.done);
+      },
+  },
+
+}
+</script>
