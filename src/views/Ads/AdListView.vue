eb77b1b35482dc2ae47af445733d3ec7f02f52c3
anarhisssttt <grebeneva.y.i.23@gmail.com>
2025-04-10 18:43:16 +0300

    Верстка страницы списка объявлений пользователя

--- src/views/Ads/AdListView.vue
+++ src/views/Ads/AdListView.vue
@@ -1,16 +1,41 @@
+
 <template>
-    <v-container>
-      <v-row justify="center">
-        <h1>AdList</h1>
-      </v-row>
-    </v-container>
-  </template>
-  
-  <script>
-  export default {
-    data () {
-      return {
-      }
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12" sm="8" lg="6">
+        <h1 class="text--secondary mb-3 mt-3">My ads</h1>
+        <v-card v-for="ad in ads" :key="ad.id" class="mb-3" max-width="1000">
+          <v-row>
+              <v-col xs="4">
+                  <v-img :src="ad.src" height="175px" cover></v-img>
+              </v-col>
+              <v-col xs="8">
+                  <h2 class="text--primary">{{  }}</h2>
+                  <p style="height: 85px; overflow: hidden; text-overflow: ellipsis;">{{  }}</p>
+                  <v-card-actions>
+                      <v-spacer></v-spacer>
+                      <v-btn color="primary" variant="text" :to="Open">
+                          Open
+                      </v-btn>
+                  </v-card-actions>
+              </v-col>
+          </v-row>
+        </v-card>>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: "",
+    desc:"",
+    promo: true,
+    src: "",
+    id:""
     }
   }
-  </script>
+}
+</script>
