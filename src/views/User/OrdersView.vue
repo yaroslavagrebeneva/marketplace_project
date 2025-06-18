a141a885cd84827ebc021027d2e7e1afa795fc31
anarhisssttt <grebeneva.y.i.23@gmail.com>
2025-06-19 00:04:24 +0300

    yasya_p_6

--- src/views/User/OrdersView.vue
+++ src/views/User/OrdersView.vue
@@ -1,63 +1,85 @@
 <template>
     <v-container>
       <v-row justify="center">
-      <v-col cols="12" sm="8" lg="6">
-        <h1 class="text--secondary mb-3 mt-3">Orders</h1>
-        <v-list subheader two-line flat>
-            <v-list-item v-for="order in orders"
-            :key="order.id">
+        <v-col cols="12" sm="8" lg="6">
+          <h1 class="text--secondary mb-3 mt-3">Orders</h1>
+          <v-list subheader two-line flat>
+            <v-item-group v-model="selectedOrders">
+              <v-list-item
+                v-for="order in orders"
+                :key="order.id"
+                :value="order.id"
+              >
                 <template v-slot:prepend>
-                    <v-list-item-action>
-                        <v-checkbox
-                            :input-value="order.done"
-                            color="primary"
-                            @click="markDone(order)"
-                        ></v-checkbox>
-                    </v-list-item-action>
+                  <v-list-item-action>
+                    <v-checkbox
+                      v-model="order.done"
+                      color="primary"
+                      hide-details
+                      @change="markDone(order)"
+                    />
+                  </v-list-item-action>
                 </template>
-
                 <v-list-item-title>{{ order.name }}</v-list-item-title>
                 <v-list-item-subtitle>{{ order.phone }}</v-list-item-subtitle>
-
                 <template v-slot:append>
-                    <v-list-item-action>
-                        <v-btn class="primary" :to="'/ad/' + order.adld">Open</v-btn>
-                    </v-list-item-action>
+                  <v-list-item-action>
+                    <!-- Обновлена кнопка с явным классом для primary цвета -->
+                    <v-btn class="primary" :to="'/ad/' + order.adId" color="primary">
+                      Open
+                    </v-btn>
+                  </v-list-item-action>
                 </template>
-            </v-list-item>
-        </v-list>
-      </v-col>  
+              </v-list-item>
+            </v-item-group>
+          </v-list>
+        </v-col>
       </v-row>
     </v-container>
-</template>
+  </template>
   
-<script>
-export default {
+  <script>
+  export default {
     data() {
-        // return {
-        //     orders: [
-        //         {
-        //             id: "123",
-        //             name: "Kate",
-        //             phone: "+7(978)774-57-96",
-        //             adId: "1",
-        //             done: true
-        //         }
-        //     ]
-        // }
+      return {
+        selectedOrders: [],
+        orders: [
+          {
+            id: "123",
+            name: "Kostya",
+            phone: "+7(978)000-00-05",
+            adId: "1",
+            done: true,
+          },
+          {
+            id: "124",
+            name: "Ivan",
+            phone: "+7(978)000-00-06",
+            adId: "2",
+            done: false,
+          },
+          {
+            id: "125",
+            name: "Anna",
+            phone: "+7(978)000-00-07",
+            adId: "3",
+            done: true,
+          },
+          {
+            id: "126",
+            name: "Elena",
+            phone: "+7(978)000-00-08",
+            adId: "4",
+            done: false,
+          },
+        ],
+      };
     },
-
     methods: {
-        markDone(order) {
-            order.done = !order.done
-            console.log(order.done)
-        }
+      markDone(order) {
+        console.log(`Заказ ${order.id} помечен как ${order.done ? "обработан" : "необработан"}`);
+      },
     },
-
-    computed: {
-        orders() {
-            return this.$store.getters.orders
-        }
-    },
-}
-</script>
+  };
+  </script>
+  
